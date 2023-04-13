85e2d1e30f064179b20c04715587d4660e3a31ad
IsaacLee <34433778+cotpy1@users.noreply.github.com>
2023-04-13 10:04:11 +0900

    first commit

---
+++ app/src/main/java/com/example/ideer/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.ideer;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.ideer.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.MediaType;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding; //뷰바인딩 객체 생성
+    List<Message> messageList;
+    MessageAdapter  messageAdapter;
+
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS).build();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding =  ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot(); //바인딩한 객체를 view에 할당
+        setContentView(view);
+        messageList = new ArrayList<>();
+
+        messageAdapter = new MessageAdapter(messageList);
+        binding.recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        binding.recyclerView.setLayoutManager(llm);
+
+        binding.sendBtn.setOnClickListener((v)-> {
+            String question = binding.messageEditText.getText().toString().trim();
+            if(question.isEmpty()){
+            }else{
+                addToChat(question,Message.SENT_BY_ME);
+                binding.messageEditText.setText("");//입력값이 null일때 입력을 방지하는 부분이 필요
+                callAPI(question);
+            }
+
+        });
+    }
+
+    void addToChat(String message,String sentBy){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message,sentBy));
+                messageAdapter.notifyDataSetChanged();
+                binding.recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+
+            }
+        });
+    }
+    void addResponse(String response){
+        messageList.remove(messageList.size()-1);
+        addToChat(response,Message.SENT_BY_BOT);
+    }
+
+    void callAPI(String question){
+        //okhttp
+        messageList.add(new Message("입력중..",Message.SENT_BY_BOT));
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("model","text-davinci-003");
+            jsonBody.put("prompt",question);
+            jsonBody.put("max_tokens",4000);
+            jsonBody.put("temperature",0);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization","Bearer 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")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("좀만 더 생각해 볼께요 조금 이따 다시 물어봐주세요^^");
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        addResponse(result.trim());
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }else{
+                    addResponse("좀만 더 생각해 볼께요 조금 이따 다시 물어봐주세요^^");
+
+                }
+
+            }
+        });
+    }
+}
