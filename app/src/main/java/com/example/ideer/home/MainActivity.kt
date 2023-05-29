7644cf9691ddee644c1cd5323405b71d3cd513c1
woosungwon <wkehdck753@naver.com>
2023-05-29 21:37:03 +0900

    notice 추가,아이콘 생성

--- app/src/main/java/com/example/ideer/home/MainActivity.kt
+++ app/src/main/java/com/example/ideer/home/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ideer.home
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.PopupMenu
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ideer.R
 import com.example.ideer.chat.ChatActivity
@@ -25,6 +27,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
         binding.startMenuBtn.setOnClickListener { v -> showMenuOptions() }
+        binding.noticeInfo.setOnClickListener {
+            showNoticePopup()
+        }
     }
 
     private fun showMenuOptions() {
@@ -48,5 +53,21 @@ class MainActivity : AppCompatActivity() {
 
         // 메뉴가 표시되도록 명시적으로 호출
         popupMenu.show()
+    }
+
+    private fun showNoticePopup() {
+        val noticeText = "본 앱이 제공하는 모든 자료는 데이터마이닝에 기반하며, 기반 자료의 원 저작자의 저작권은 저작권법 제2장 제6조에 의해 보호됩니다. 본 앱으로 생성한 아이디어를 영리적으로 이용하는 것은 저작권의 침해로 인정될 수 있으며 이를 확인 하지 않은 앱 이용에 의한 앱 이용자의 피해에 대한 책임은 앱 이용자에게 있습니다.\n\n" +
+                "① 편집저작물은 독자적인 저작물로서 보호된다. <개정 2003·5·27>\n" +
+                "② 편집저작물의 보호는 그 편집저작물의 구성부분이 되는 소재의 저작권 그 밖의 이 법에 의하여 보호되는 권리에영향을 미치지 아니한다. <개정 2003·5·27>"
+
+        val alertDialog = AlertDialog.Builder(this)
+                .setTitle("알림")
+                .setMessage(noticeText)
+                .setPositiveButton("확인") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+
+        alertDialog.show()
     }
 }
